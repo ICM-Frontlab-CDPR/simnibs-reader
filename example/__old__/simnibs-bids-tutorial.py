7286baa5d37c77f74a3cda13e75de1d52409fc34
hippolyte456 <hippolytedreyfus@gmail.com>
2026-09-10 12:43:32 +0000

    Deployed 908850d with MkDocs version: 1.6.1

---
+++ example/__old__/simnibs-bids-tutorial.py
@@ -0,0 +1,10 @@
+
+# ADD to pybids plutot 
+# - test de l'outil de querying d’un sous ensemble des folder de simulation (based on name folder or other ?)
+
+import simnibs-bids as snb   # higher level : gerer les experiences multiples etc
+
+
+snb.query( by-targets , by-a-simu-modality) 
+#--> renvoie les listes de folders concernées (c'est peutetre pas tres utile si les differentes simulations sont bine rangées: )
+# path : simnibs-simu/simnibs-param/simu-subject-1...
